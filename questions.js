04eec322914e695a5347f9124fb37b9aac9db4a0
gouse2003 <151511583+gouse2003@users.noreply.github.com>
2023-11-20 14:09:38 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+//creating an array and passing the numbers, questions, options and answers
+let questions = [
+    {
+        numb: 1,
+        question: "why HTML is more important for creating web pages",
+        answer: "4. Both 1 & 2",
+        options: [
+            "1. It contains the information that tells the browser",
+            "2. Terms of links,texts,where to find images",
+            "3. It is used for multipurpose for creating pages",
+            "4. Both 1 & 2"
+        ]
+    },
+    {
+        numb: 2,
+        question: "what does PHP stands for?",
+        answer: "1. Hypertext Preprocessor",
+        options: [
+            "1. Hypertext Preprocessor",
+            "2. Hypertext Programming",
+            "3. Hypertext Preprogramming",
+            "4. Hometext Preprocessor"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the purpose of the meta tag in HTML, and how is it commonly used?",
+        answer: "3. meta charset=UTF-8",
+        options: [
+            "1. meta Charset=UTF-8",
+            "2. meta charSet=UTF-8",
+            "3. meta charset=UTF-8",
+            "4. meta charset=UTF-6"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What Does CSS Stands For?",
+        answer: "4. Cascading Style Sheet",
+        options: [
+            "1. Common Style Sheet",
+            "2. Colourful Style Sheet",
+            "3. Computer Style Sheet",
+            "4. Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What Does SQL Stands For?",
+        answer: "4. Structured Query Language",
+        options: [
+            "1. Stylish Question Language",
+            "2. Stylesheet Query Language",
+            "3. Statement Question Language",
+            "4. Structured Query Language"
+        ]
+    },
+];
